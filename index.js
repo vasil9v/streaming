0397e020a14bb4272a7c957da7ca2afc3cffdb83
Vasilios Daskalopoulos <vasil9v@gmail.com>
2025-03-25 09:17:13 -0400

    added a websocket streaming connection with an out of band /rate api to set data volume rate

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import http from 'node:http';
+import { WebSocketServer } from 'ws';
 import fs from 'node:fs';
 import querystring from 'node:querystring';
 import { open } from 'lmdb';
+
+const wss = new WebSocketServer({ port: 9001 });
 
 const myDB = open({
   path: 'my-stream-db',
@@ -37,6 +40,51 @@ function testCollectDistinct() {
   distinctCache = {};
 }
 
+function getRecords(start, end, cb) {
+  // let results = myDB.getRange({start: start, end: end, limit: 1000, sArray: true });
+  let results =[];
+  myDB.getRange({start: start, end: end, limit: 1000, sArray: true })
+  // .filter(({ key, value }) => test(key)) // TODO
+  .forEach(({ key, value }) => {
+    collectDistinct(value);
+    results.push({key: key, value: value});
+  });
+  cb && cb(results, results.length);
+}
+
+let config = {
+  ptr: 0,
+  size: 100,
+  period: 500
+};
+
+function streamChunk() {
+  console.log('ptr: ' + config.ptr);
+  getRecords(config.ptr, config.ptr + config.size, function (results, items) {
+    wss.clients.forEach(client => {
+      client.send(JSON.stringify(results));
+    });
+    config.ptr += items;
+    if (items < config.size) {
+      config.ptr = 0;
+    }
+  });
+  setTimeout(streamChunk, config.period);
+}
+
+wss.on('connection', ws => {
+  console.log('New client connected!');
+  // ws.send('connection established');
+  ws.on('close', () => console.log('Client has disconnected!'));
+  ws.on('message', data => {
+    console.log('message: ' + data);
+    setTimeout(streamChunk, config.period);
+  });
+  ws.onerror = function () {
+    console.log('websocket error');
+  };
+});
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
@@ -60,21 +108,24 @@ const server = http.createServer((req, res) => {
     return;
   }
 
+  if (req.url.startsWith('/rate')) {
+    let query = querystring.parse(req.url.split('?')[1]);
+    config.size = Number(query.size || 100);
+    config.period = Number(query.period || 500);
+    console.log('config: ' + JSON.stringify(config));
+    res.end(JSON.stringify(config));
+    return;
+  }
+
   if (req.url.startsWith('/stream')) {
     let query = querystring.parse(req.url.split('?')[1]);
     let start = Number(query.start || 0);
     let end = Number(query.end || 100);
-    // let results = myDB.getRange({start: start, end: end, limit: 1000, sArray: true });
-    let results =[];
-    myDB.getRange({start: start, end: end, limit: 1000, sArray: true })
-      // .filter(({ key, value }) => test(key)) // TODO
-      .forEach(({ key, value }) => {
-        collectDistinct(value);
-        results.push({key: key, value: value})
-      });
-    res.end(JSON.stringify(results));
+
+    getRecords(start, end, function (results, items) {
+      res.end(JSON.stringify(results));
+    });
     return;
-    // response.write() response.writeContinue() - https://nodejs.org/api/http.html#responsewritechunk-encoding-callback
   }
 
   if (req.url.startsWith('/distinct')) {
